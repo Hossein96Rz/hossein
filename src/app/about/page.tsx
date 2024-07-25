7471dbd28f79fd1d0ea1986d4b0692b236961632
Hossein96Rz <hosseinfaster@yahoo.com>
2024-07-25 16:14:08 +0330

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "About page",
+};
+
+export default function About() {
+  return <p>this is about page</p>;
+}
---
+++ src/dictionaries.tsx
@@ -0,0 +1,6 @@
+const dictionaries: any = {
+  en: () => import("./dictionaries/en.json").then((module) => module.default),
+  fa: () => import("./dictionaries/fa.json").then((module) => module.default),
+};
+
+export const getDictionary = async (locale: any) => dictionaries[locale]();
