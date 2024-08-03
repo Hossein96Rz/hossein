1db818e10a1a3bbcaf6ee8d713b891395fea4e5d
Hossein96Rz <hosseinfaster@yahoo.com>
2024-08-03 17:33:43 +0330

    modal completed

--- src/views/Modal.jsx
+++ src/views/Modal.jsx
@@ -1,20 +1,37 @@
 "use client";
 
+import classNames from "classnames";
 import { useEffect } from "react";
 import { createPortal } from "react-dom";
 
 function Modal({ children, modal, toggle }) {
   const ModalNode = (
-    <div id="portal">
-      <div className="relative top-1/2 inline-block -translate-y-1/2 rounded-md bg-white px-3 pt-8 ltr:left-1/2 ltr:-translate-x-1/2 rtl:right-1/2 rtl:translate-x-1/2">
+    <div
+      id="portal"
+      className={classNames("transition-all duration-300", {
+        "z-[99999] opacity-100": modal,
+        "z-0 opacity-0": !modal,
+      })}
+    >
+      {modal && (
         <div
-          className="absolute right-2 top-2 flex h-6 w-6 items-center justify-center rounded-full border-2 border-black"
-          onClick={() => toggle()}
+          className={classNames(
+            "relative top-1/2 inline-block -translate-y-1/2 rounded-md bg-white px-3 pt-8 transition-all ltr:left-1/2 ltr:-translate-x-1/2 rtl:right-1/2 rtl:translate-x-1/2 dark:bg-[#12161f]",
+            {
+              "opacity-0": !modal,
+              "opacity-100": modal,
+            },
+          )}
         >
-          X
+          <div
+            className="absolute right-2 top-2 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full border-2 border-black dark:border-white dark:text-white"
+            onClick={() => toggle()}
+          >
+            X
+          </div>
+          {children}
         </div>
-        {children}
-      </div>
+      )}
     </div>
   );
 
@@ -30,7 +47,7 @@ function Modal({ children, modal, toggle }) {
     return () => document.removeEventListener("click", outSideClickHandler);
   }, [modal]);
 
-  return modal && createPortal(ModalNode, document.body);
+  return createPortal(ModalNode, document.body);
 }
 
 export default Modal;
