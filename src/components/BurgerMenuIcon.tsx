7bfb807cc41f10ee9d9747c00e495910cce63742
Hossein96Rz <hosseinfaster@yahoo.com>
2024-08-26 17:11:03 +0330

    conflict

--- src/components/BurgerMenuIcon.tsx
+++ src/components/BurgerMenuIcon.tsx
@@ -9,7 +9,10 @@ interface BurgerMenuIconProps {
 const BurgerMenuIcon = ({ isOpen, toggle }: BurgerMenuIconProps) => {
   return (
     <div className="relative h-full w-full rounded-full" onClick={toggle}>
-      <button className="relative z-10 flex h-full w-full flex-col items-center justify-center space-y-1 focus:outline-none">
+      <button
+        aria-label="sidebar button"
+        className="relative z-10 flex h-full w-full flex-col items-center justify-center space-y-1 focus:outline-none"
+      >
         <div
           className={classNames(
             "h-0.5 w-6 transform bg-white transition-transform duration-300 ease-in-out dark:bg-black",
